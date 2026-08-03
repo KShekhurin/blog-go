34521a2a7c0352b4a69587e259a1f52839b27c37
Shekhurin Kirill <k.shekhurin@gmail.com>
2026-08-03 22:28:52 +0300

    fixed goose migration datarace

--- migrations/00004_removed_is_deleted_from_post.sql
+++ migrations/00004_removed_is_deleted_from_post.sql
@@ -4,4 +4,4 @@ DROP COLUMN IF EXISTS is_deleted;
 
 -- +goose Down
 ALTER TABLE posts
-ADD COLUMN IF NOT EXISTS is_deleted bool not null default false
+ADD COLUMN IF NOT EXISTS is_deleted bool not null default false;
